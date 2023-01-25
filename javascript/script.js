c1bd025542cb4f8916649e6ee3bf78cb98c290de
joseedivandro <jose.edivandro.ja@outlook.com>
2023-01-25 18:31:20 -0300

    adicionando funcao que pega quizzes armazenados na api

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,10 @@
+let ArmazenaQuizz = document.querySelector('.agrupaQuizzes');
+
 function criarQuizz(mudar) {
     mudar = document.querySelector('.container_principal')
     mudar.classList.add('desativado');
-
     mudar = document.querySelector('.container-criacao')
     mudar.classList.remove('desativado')
-
     mudar = document.querySelector('.pagina1-criacao')
     mudar.classList.remove('desativado')
 }
@@ -12,7 +12,6 @@ function criarQuizz(mudar) {
 function passarPagDois(mudar){
     mudar = document.querySelector('.pagina1-criacao')
     mudar.classList.add('desativado');
-
     mudar = document.querySelector('.pagina2-criacao')
     mudar.classList.remove('desativado')
 }
@@ -20,8 +19,36 @@ function passarPagDois(mudar){
 function passarPagTres(mudar){
     mudar = document.querySelector('.pagina2-criacao')
     mudar.classList.add('desativado');
-
     mudar = document.querySelector('.pagina3-criacao')
     mudar.classList.remove('desativado')
-
 }
+
+
+function puxaQuizz(){
+    const promise = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
+
+  promise.then(percorre);
+}
+
+function sucessoQuizz(promise){
+    console.log(promise)
+}
+
+
+
+function percorre (promise){
+    for(i=0; i<promise.data.length; i++){
+
+        let id = promise.data[i].id;
+        let titulo = promise.data[i].title;
+        let imagem = promise.data[i].image;
+
+        ArmazenaQuizz.innerHTML +=`<div class="quizz"><img src="${imagem}" alt="">
+        <h3> ${titulo}</h3>
+    </div>`
+        
+    }
+    
+}
+
+puxaQuizz();
